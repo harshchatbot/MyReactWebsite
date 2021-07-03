ec7067f1e566244491188b8095e8663deb3016f8
Harsh veer Nirwan <harshveernirwan@gmail.com>
2021-07-03 21:21:15 +0530

    adding form

---
+++ src/MyComponents/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Form = () => {
+    return (
+        <div>
+            <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
+        </div>
+    )
+}
